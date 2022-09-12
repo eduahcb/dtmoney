8b0b1207e1f6a06da959136450bfd4b17f379e81
eduahcb <dev_eduardo@hotmail.com>
2022-09-12 18:34:16 -0300

    feat: creating Brand component (style, stories, tests)

---
+++ src/components/atoms/Brand/Brand.stories.tsx
@@ -0,0 +1,14 @@
+import React, { ReactElement } from 'react'
+
+import { Brand } from 'components/atoms/Brand'
+
+export default {
+  title: 'Components/Brand',
+  component: Brand
+}
+
+export const usage = (): ReactElement => (
+  <div style={{ backgroundColor: 'var(--blue)', padding: '8px' }}>
+    <Brand />
+  </div>
+)
