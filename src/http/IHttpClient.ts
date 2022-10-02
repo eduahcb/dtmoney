1f1f28dda4dd0b60580d3db2812357e9d3125443
eduahcb <dev_eduardo@hotmail.com>
2022-10-02 18:57:10 -0300

    chore: removing unnecessary files

--- src/http/IHttpClient.ts
+++
@@ -1,5 +0,0 @@
-/* eslint-disable @typescript-eslint/no-invalid-void-type */
-export interface IHttpClient {
-  get: <T = any > (path: string) => Promise<T>
-  post: <T = any> (path: string, payload: any) => Promise<T>
-}
