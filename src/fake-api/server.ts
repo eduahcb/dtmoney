2fec91cf86ea076b0f32d0775fb97da637dd93cf
eduahcb <dev_eduardo@hotmail.com>
2022-10-08 18:53:49 -0300

    chore: adding environment property

--- src/fake-api/server.ts
+++ src/fake-api/server.ts
@@ -3,8 +3,9 @@ import { createServer, Server } from 'miragejs'
 import { models } from './models'
 import { factories } from './factories'
 
-export const fakeApi = (): Server => {
+export const fakeApi = (environment: 'development' | 'test' = 'development'): Server => {
   return createServer({
+    environment,
     models,
 
     factories,
@@ -19,6 +20,12 @@ export const fakeApi = (): Server => {
       this.get('/transactions', (schema) => ({
         transactions: schema.all('transaction').models
       }))
+
+      // Error
+
+      // this.get('/transactions', (schema) => {
+      //   return new Response(500, {}, { error: 'kkk' })
+      // })
     }
   })
 }
