de5aabea95198ee3aec0d7b6598b5debb1be6379
eduahcb <dev_eduardo@hotmail.com>
2022-10-08 13:48:11 -0300

    test: creating test-utils file

---
+++ src/test-utils.tsx
@@ -0,0 +1,41 @@
+/* eslint-disable import/export */
+import React, { ReactElement, ReactNode } from 'react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import {
+  render,
+  renderHook,
+  RenderHookOptions,
+  RenderHookResult,
+  RenderOptions,
+  RenderResult
+} from '@testing-library/react'
+
+type TestProps = {
+  children: ReactNode
+}
+
+const queryClient = new QueryClient()
+
+const TestProvider = ({ children }: TestProps): ReactElement => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      { children }
+    </QueryClientProvider>
+  )
+}
+
+const customRender = (
+  ui: ReactElement,
+  options?: Omit<RenderOptions, 'wrapper'>
+): RenderResult => render(ui, { wrapper: TestProvider, ...options })
+
+const customRenderHook = <
+  Result,
+  Props
+,>(
+    render: (initialProps: Props) => Result,
+    options?: Omit<RenderHookOptions<Props>, 'wrapper'>
+  ): RenderHookResult<Result, Props> => renderHook(render, { wrapper: TestProvider, ...options })
+
+export * from '@testing-library/react'
+export { customRender as render, customRenderHook as renderHook }
